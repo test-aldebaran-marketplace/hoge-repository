100fb9de652ad96c09b0bb0ca3f15f20e654d745
t.kawatsu <t.kawatsu@signate.co.jp>
2018-12-06 10:33:45 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+# TODO: FIX
+# import Algorithmia
+
+# API calls will begin at the apply() method, with the request body passed as 'input'
+# For more details, see hogehogehoge.com/hugahuga
+def apply(input):
+    return "hello world"
